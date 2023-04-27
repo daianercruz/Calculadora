4148ff22b2cea06c9c90fbfb9587c6e4cea76ccd
Daiane Cruz <daianercruz12@gmail.com>
2023-04-27 16:55:16 -0300

    Iniciando o js

---
+++ scripts.js
@@ -0,0 +1,3 @@
+const operacaoprev = document.querySelector("#operacao-prev")
+const operacaocorre = document.querySelector ("operacao-corrente")
+const botoes = document.querySelector ("#botoes button")
