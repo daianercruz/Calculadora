634c08cb7d71d344fd5626d46c8761c04683fd96
Daiane Cruz <daianercruz12@gmail.com>
2023-05-01 20:00:30 -0300

    Numero aparecendo no display da calculadora

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
-const operacaoprev = document.querySelector("#operacao-prev")
-const operacaocorre = document.querySelector ("operacao-corrente")
-const botoes = document.querySelector ("#botoes button")
+const previousOperationText = document.querySelector("#operacao-prev");
+const currentOperationText = document.querySelector("#operacao-corrente");
+const buttons = document.querySelectorAll("#botoes button");
+
+class Calculator {
+ constructor(previousOperationText,currentOperationText){
+ this.previousOperationText = previousOperationText;
+ this.currentOperationText = currentOperationText;
+ this.currentOperation = "";
+
+}
+//Criando a tela da calculadora 
+addDigit(digit){
+
+
+this.currentOperation = digit;
+
+this.updateScreen();
+}
+
+updateScreen(){
+    this.currentOperationText.innerText += this.currentOperation;
+}
+}
+
+const calc = new Calculator(previousOperationText,currentOperationText);
+
+buttons.forEach((btn)=>{
+   btn.addEventListener("click",(e) =>{
+    const value =e.target.innerText;
+
+    if(+value>=0 ||value==="."){
+
+        calc.addDigit(value)
+
+    }else{
+        console.log("op:" + value);
+    }
+
+   })
+
+
+});
+
